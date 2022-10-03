61b3c568e8c6eaef8fdf11bf421443dce008ba66
volodymyr_krokhmaliuk <krokhmalyuk777@gmail.com>
2022-10-03 21:03:10 +0300

    test1

--- src/hilell/test/TestMain.java
+++ src/hilell/test/TestMain.java
@@ -1,2 +1,7 @@
-package hilell.test;public class TestMain {
+package hilell.test;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("It's test main!");
+    }
 }
