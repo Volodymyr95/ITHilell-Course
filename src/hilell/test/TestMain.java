63145216b6f0062b94305b93a000eb7d940b9f0c
volodymyr_krokhmaliuk <krokhmalyuk777@gmail.com>
2022-10-06 19:19:36 +0300

    refactor

--- src/hilell/test/TestMain.java
+++
@@ -1,7 +0,0 @@
-package hilell.test;
-
-public class TestMain {
-    public static void main(String[] args) {
-        System.out.println("It's test main!");
-    }
-}
--- src/hilell/test1/Test1Main.java
+++
@@ -1,7 +0,0 @@
-package hilell.test1;
-
-public class Test1Main {
-    public static void main(String[] args) {
-
-    }
-}
